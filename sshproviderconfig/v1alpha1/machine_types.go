2b67ceda303d17d4dafb292cddf9c6cf418bae78
Daniel Lipovetsky <daniel@platform9.com>
2018-06-20 13:00:36 -0700

    Add MaxPods to SSHMachineProviderConfig

--- sshproviderconfig/v1alpha1/machine_types.go
+++ sshproviderconfig/v1alpha1/machine_types.go
@@ -11,6 +11,16 @@ import (
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type SSHMachineProviderConfig struct {
 	metav1.TypeMeta `json:",inline"`
+
+	// maxPods is the number of pods that can run on this Kubelet.
+	// Dynamic Kubelet Config (beta): If dynamically updating this field, consider that
+	// changes may cause Pods to fail admission on Kubelet restart, and may change
+	// the value reported in Node.Status.Capacity[v1.ResourcePods], thus affecting
+	// future scheduling decisions. Increasing this value may also decrease performance,
+	// as more Pods can be packed into a single node.
+	// Default: 110
+	// +optional
+	MaxPods int32 `json:"maxPods,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
